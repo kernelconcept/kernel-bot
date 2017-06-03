e68a0441acc436f5fde11596f56cb2ee23da11aa
Afranche Gaia <abridgedkirigami@gmail.com>
2017-06-03 14:36:59 +0200

    Image generation fix (bad syntax which would lead to bugs on people with a / in their name) + PEP-8 compliance for image.py file

--- kernel/image.py
+++ kernel/image.py
@@ -1,11 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 from urllib.request import Request, urlopen
 from textwrap import wrap
+import discord
 import os
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__)) + '/..'
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko)'
+                         ' Chrome/23.0.1271.64 Safari/537.11',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
            'Accept-Encoding': 'none',
@@ -13,7 +15,7 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KH
            'Connection': 'keep-alive'}
 
 
-def generate_avatar(avatar_url, id):
+def generate_avatar(avatar_url, profile_id):
     base = Image.open(BASE_DIR + '/templates/base.png')
     r = Request(avatar_url, headers=HEADERS)
     content = urlopen(r).read()
@@ -22,11 +24,11 @@ def generate_avatar(avatar_url, id):
         f.close()
     avatar = Image.open(BASE_DIR + '/pictures/treating.png').resize((1148, 1148))
     base.paste(avatar, (0, 0))
-    base.save(BASE_DIR + '/pictures/temp_{}.png'.format(id))
-    return BASE_DIR + '/pictures/temp_{}.png'.format(id)
+    base.save(BASE_DIR + '/pictures/temp_{}.png'.format(profile_id))
+    return BASE_DIR + '/pictures/temp_{}.png'.format(profile_id)
 
 
-def generate_profile(id: str,
+def generate_profile(profile: discord.Member,
                      profile_name: str,
                      profile_title: str,
                      profile_desc: str,
@@ -35,7 +37,7 @@ def generate_profile(id: str,
                      profile_thanks: int):
     base = Image.open(BASE_DIR + '/templates/baseWhite.png')
     avatar_mask = Image.open(BASE_DIR + '/templates/avatarMask.png').convert('L')
-    avatar_link = generate_avatar(avatar, id)
+    avatar_link = generate_avatar(avatar, profile.id)
     avatar = Image.open(avatar_link)
 
     if profile_disp:
@@ -64,12 +66,13 @@ def generate_profile(id: str,
     draw.text((1150, 200), '{}'.format(profile_title), font=subtitle, fill=(100, 100, 100, 255))
     draw.text((1080, 990), '{}'.format(profile_thanks), font=thanks, fill=(246, 78, 52, 255))
     draw.text((1090 + thanks_x, 990), 'R'.format(profile_thanks), font=thanks, fill=(246, 78, 52, 255))
-    draw.text((1145 + thanks_x, 1015), 'EMERCIEMENTS'.format(profile_thanks), font=thanks_little, fill=(246, 78, 52, 255))
+    draw.text((1145 + thanks_x, 1015), 'EMERCIEMENTS'.format(profile_thanks), font=thanks_little,
+              fill=(246, 78, 52, 255))
     description = wrap(profile_desc, width=38)
     base_y = 300
     for line in description:
         draw.text((1150, base_y), '{}'.format(line), font=desc, fill=(0, 153, 204, 255))
         base_y += 70
-    base.save(BASE_DIR + '/pictures/{}.png'.format(id))
+    base.save(BASE_DIR + '/pictures/{}.png'.format(profile.id))
     os.remove(avatar_link)
-    return BASE_DIR + '/pictures/{}.png'.format(id)
+    return BASE_DIR + '/pictures/{}.png'.format(profile.id)
