7cc7c6045d8bb474ef71f35594fc2571f75cf2b6
Afranche Gaia <abridgedkirigami@gmail.com>
2017-07-03 17:16:21 +0200

    Fix on previous merge

--- kernel.py
+++ kernel.py
@@ -14,19 +14,8 @@ if __name__ == '__main__':
     if not path.isfile('.token'):
         prompt_token()
     token = open('.token', 'r').read()
-<<<<<<< HEAD
-    bot = KernelBot(api_token=token)
-    bot.add_cog(
-        Commands(bot)
-    )
-    bot.add_cog(
-        Profile(bot)
-    )
+    db = load()
+    bot = KernelBot(api_token=token, redis=db)
     while True:  # TODO: Méthode paysanne pour relancer le bot on failure.
         if not bot.is_logged_in:
             bot.run()
-=======
-    db = load()
-    bot = KernelBot(token, db)
-    bot.run()
->>>>>>> e9314225b796755e5e6b58e9bcf2020874809cd2
